3772afefd5779540a799172df65ade4247003b9c
Praneshk12 <77246096+Praneshk12@users.noreply.github.com>
2021-10-03 10:44:25 -0700

    Update App.js

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from "./screens/Home";
-import StartMapScreen from "./screens/StarMap";
-import DailyPicScreen from "./screens/DailyPic";
-import SpaceCraftScreen from "./screens/SpaceCraft";
+import StartMapScreen from "./StarMap";
+import DailyPicScreen from "./DailyPic";
+import SpaceCraftScreen from "./SpaceCraft";
 
 const Stack = createStackNavigator();
 
